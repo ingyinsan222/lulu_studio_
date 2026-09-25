ca6ac48da72db9646fe4f3d0e75322a8bca88733
Ingyin San <ingyinsan.ingyinsan22@gmail.com>
2026-09-25 21:28:34 +0630

    Update data.js

--- js/data.js
+++ js/data.js
@@ -13,8 +13,8 @@ const CREATOR = { name: "LuLuStudio", tagline: "Small stickers, made slowly." };
 
 const PACKS = [
   { id: "mood",  name: "Daily Mood",   emoji: "", cover: true, stickerFiles: 5, price: 0, stickers: 5, preview: 5, telegram:"https://t.me/addstickers/ddaily_moodd", desc: "Everyday reactions — tired, hyped, done with today." },
-  { id: "cat",   name: "Chaos",    emoji: "", cover: true, stickerFiles: 10, price: 3.99, stickers: 10, preview: 5,  desc: "Cats being dramatic about very small things." },
-  { id: "soft",  name: "Coming Soon..",    emoji: "", price: 0.00, stickers: 22, preview: 5,  desc: "Gentle little moments for slow, cozy chats." },
-  { id: "study", name: "Coming Soon..",  emoji: "", price: 0.00, stickers: 30, preview: 6,  desc: "Deadlines, coffee, and tiny wins for study days." },
-  { id: "love",  name: "Coming Soon..",emoji: "", price: 0.00, stickers: 36, preview: 7,  desc: "Hearts and small messages for someone you like." }
+  { id: "cat",   name: "Chaos",    emoji: "", cover: true, stickerFiles: 10, price: 3.99, stickers: 10, preview: 5,  desc: "Carr being dramatic about very small things." },
+  { id: "soft",  name: "Coming Soon..",    emoji: "", price: 0.00, stickers: 0, preview: 5,  desc: "Gentle little moments for slow, cozy chats." },
+  { id: "study", name: "Coming Soon..",  emoji: "", price: 0.00, stickers: 0, preview: 6,  desc: "Deadlines, coffee, and tiny wins for study days." },
+  { id: "love",  name: "Coming Soon..",emoji: "", price: 0.00, stickers: 0, preview: 7,  desc: "Hearts and small messages for someone you like." }
 ];
